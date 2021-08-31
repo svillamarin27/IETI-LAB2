11ffd529c4c10ca0ef2add71f23ec91523730654
Sebastián Villamarín Rodríguez <sebastian.villamarin@mail.escuelaing.edu.co>
2021-08-31 15:50:57 -0500

    userDocument

--- src/main/java/org/ada/school/repository/UserDocument.java
+++ src/main/java/org/ada/school/repository/UserDocument.java
@@ -1,4 +1,23 @@
 package org.ada.school.repository;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
+@Document
 public class UserDocument {
+    @Id
+    String id;
+
+    String name;
+
+    @Indexed( unique = true )
+    String email;
+
+    String lastName;
+
+    Date createdAt;
+
+    public UserDocument(){}
 }
